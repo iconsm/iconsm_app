370cda8126dd42dcbbad8e6a6369abf2b9f0bf06
iconde <ikercondeperez@gmail.com>
2019-05-19 19:56:34 +0200

    Poll functionality added v1

--- app/src/main/java/com/example/alaiapp/Poll.java
+++ app/src/main/java/com/example/alaiapp/Poll.java
@@ -2,6 +2,7 @@ package com.example.alaiapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
@@ -13,9 +14,32 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
 
 public class Poll extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    FirebaseRemoteConfig mFirebaseRemoteConfig;
+    TextView textView_poll_subject;
+    TextView textView_option1;
+    TextView textView_option2;
+    TextView textView_option3;
+    TextView textView_option4;
+
+    RadioGroup radioGroup;
+    RadioButton radioButton;
+    String receiverList = "alaiaktaldeaapp@gmail.com";
+    String[] receiver = {receiverList};
+    String message = "Añade aquí algún comentario extra sobre el tema encuestado.";
+    String subject;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +65,68 @@ public class Poll extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        /* Function code starts here */
+        mFirebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
+        textView_poll_subject = (TextView) findViewById(R.id.textViewPollSubject);
+        textView_option1 = (TextView) findViewById(R.id.textViewPoll_Option1);
+        textView_option2 = (TextView) findViewById(R.id.textViewPoll_Option2);
+        textView_option3 = (TextView) findViewById(R.id.textViewPoll_Option3);
+        textView_option4 = (TextView) findViewById(R.id.textViewPoll_Option4);
+
+        long cacheExpiration = 0;
+        mFirebaseRemoteConfig.fetch(cacheExpiration)
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(Poll.this, "Encuesta actualizada",
+                                    Toast.LENGTH_SHORT).show();
+
+                            // After config data is successfully fetched, it must be activated before newly fetched
+                            // values are returned.
+                            mFirebaseRemoteConfig.activateFetched();
+                        } else {
+                            Toast.makeText(Poll.this, "Actualización de encuesta fallida",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                        displayPollMessages();
+                    }
+                });
+
+        radioGroup = findViewById(R.id.radio_group);
+        Button buttonSendPoll = findViewById(R.id.button_send_poll);
+        buttonSendPoll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int radioId = radioGroup.getCheckedRadioButtonId();
+                radioButton = findViewById(radioId);
+                subject = radioButton.getText().toString();
+                sendMail();
+            }
+        });
+    }
+
+    private void displayPollMessages() {
+        textView_poll_subject.setText(mFirebaseRemoteConfig.getString("poll_subject"));
+        textView_option1.setText(mFirebaseRemoteConfig.getString("poll_option1"));
+        textView_option2.setText(mFirebaseRemoteConfig.getString("poll_option2"));
+        textView_option3.setText(mFirebaseRemoteConfig.getString("poll_option3"));
+        textView_option4.setText(mFirebaseRemoteConfig.getString("poll_option4"));
+
+    }
+
+    private void sendMail(){
+
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.putExtra(Intent.EXTRA_EMAIL,receiver);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, message);
+
+        /* This will take care of only opening email clients (Gmail, Hotmail...) */
+        intent.setType("message/rfc822");
+
+        startActivity(Intent.createChooser(intent, "Elige cómo mandar el correo"));
     }
 
     @Override
